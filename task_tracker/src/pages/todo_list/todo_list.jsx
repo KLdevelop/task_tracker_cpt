9a0995c614c9e6becd4a0a5af073466d5e697720
drapdrop1 <drapdrop1@gmail.com>
2021-02-19 15:50:26 +0300

    added hide/show elements

--- task_tracker/src/pages/todo_list/todo_list.jsx
+++ task_tracker/src/pages/todo_list/todo_list.jsx
@@ -11,14 +11,22 @@ class TodoListPage extends Component {
             {
                 name: "Разработка программного интерфейса",
                 changed: "Пон. 14:30",
-                status: "14 из 20 70%"
+                status: "14 из 20",
+                percent: "70%"
             },
             {
                 name: "Разработка программного интерфейса",
                 changed: "24.09.2020",
-                status: "Выполнен"
+                status: "Выполнен",
+                percent: "100%"
             }
-        ]
+        ],
+        arrStateLogin: true,
+        arrStateTitle: true,
+        arrStatePeriod: true,
+        arrStateSort: true,
+        arrStateShow: false,
+        arrStateInfo: false
     };
 
     onRadioChange = (e) => {
@@ -33,17 +41,60 @@ class TodoListPage extends Component {
         });
     };
 
+    onLoginClick = () => {
+        const { arrStateLogin } = this.state;
+        this.setState({
+            arrStateLogin: !arrStateLogin
+        });
+    };
+
+    onTitleClick = () => {
+        const { arrStateTitle } = this.state;
+        this.setState({
+            arrStateTitle: !arrStateTitle
+        });
+    };
+
+    onPeriodClick = () => {
+        const { arrStatePeriod } = this.state;
+        this.setState({
+            arrStatePeriod: !arrStatePeriod
+        });
+    };
+
+    onSortClick = () => {
+        const { arrStateSort } = this.state;
+        this.setState({
+            arrStateSort: !arrStateSort
+        });
+    };
+
+    onShowClick = () => {
+        const { arrStateShow } = this.state;
+        this.setState({
+            arrStateShow: !arrStateShow
+        });
+    };
+
+    onInfoClick = () => {
+        const { arrStateInfo } = this.state;
+        this.setState({
+            arrStateInfo: !arrStateInfo
+        });
+    };
+
     render() {
-        const { tasks, sortChecked, titleFilt } = this.state;
+        const { tasks, sortChecked, titleFilt, arrStateInfo, arrStatePeriod, arrStateLogin, arrStateShow,
+            arrStateTitle, arrStateSort } = this.state;
         return(
             <div className="todoPage">
                 <header>
                     <div className="headCont">
                         <h1>Task<span>Tracker</span></h1>
                         <div>
-                            <h2>
+                            <h2 onClick={ this.onLoginClick }>
                                 <>
-                                    <div className="sArrow"/>
+                                    <div className={ "sArrow" + (arrStateLogin ? "U" : "D") }/>
                                     Login
                                 </>
                             </h2>
@@ -55,40 +106,42 @@ class TodoListPage extends Component {
                 <div className="mainCont">
                     <div className="leftFilt">
                         <div className="titleFilt">
-                            <h2>
+                            <h2 onClick={ this.onTitleClick }>
                                 <>
-                                    <div className="sArrow"/>
+                                    <div className={ "sArrow" + (arrStateTitle ? "U" : "D") }/>
                                     Название
                                 </>
                             </h2>
-                            <div className="titleInp">
+                            { arrStateTitle && <div className="titleInp">
                                 <div className="loupe"/>
                                 <input value={ titleFilt } onChange={ this.onTitleChange }/>
-                            </div>
+                            </div> }
                         </div>
                         <div className="periodFilt">
-                            <h2>
+                            <h2 onClick={ this.onPeriodClick }>
                                 <>
-                                    <div className="sArrow"/>
+                                    <div className={ "sArrow" + (arrStatePeriod ? "U" : "D") }/>
                                     Период
                                 </>
                             </h2>
-                            <div className="perInp">
-                                <input type="date"/>
-                            </div>
-                            <div className="horLine"/>
-                            <div className="perInp">
-                                <input type="date"/>
-                            </div>
+                            { arrStatePeriod &&<>
+                                <div className="perInp">
+                                    <input type="date"/>
+                                </div>
+                                <div className="horLine"/>
+                                <div className="perInp">
+                                    <input type="date"/>
+                                </div>
+                            </> }
                         </div>
                         <div className="sortFilt">
-                            <h2>
+                            <h2 onClick={ this.onSortClick }>
                                 <>
-                                    <div className="sArrow"/>
+                                    <div className={ "sArrow" + (arrStateSort ? "U" : "D") }/>
                                     Сортировать по
                                 </>
                             </h2>
-                            <div className="radioSort">
+                            { arrStateSort && <div className="radioSort">
                                 <div>
                                     <input id="createTimeF" type="radio" name="sort" value="createtime"
                                         checked={ sortChecked == 'createtime' }
@@ -107,20 +160,20 @@ class TodoListPage extends Component {
                                         onChange={ this.onRadioChange }/>
                                     <label htmlFor="statusF">Статусу</label>
                                 </div>
-                            </div>
+                            </div> }
                         </div>
                         <div className="showFilt">
-                            <h2>
+                            <h2 onClick={ this.onShowClick }>
                                 <>
-                                    <div className="sArrow"/>
+                                    <div className={ "sArrow" + (arrStateShow ? "U" : "D") }/>
                                     Показывать
                                 </>
                             </h2>
                         </div>
                         <div className="infoFilt">
-                            <h2>
+                            <h2 onClick={ this.onInfoClick }>
                                 <>
-                                    <div className="sArrow"/>
+                                    <div className={ "sArrow" + (arrStateInfo ? "U" : "D") }/>
                                     Информация
                                 </>
                             </h2>
@@ -142,7 +195,12 @@ class TodoListPage extends Component {
                                         <div>
                                             <h3>{ task.name }</h3>
                                             <h3 className="greyH">{ task.changed }</h3>
-                                            <h3>{ task.status }</h3>
+                                            <h3>
+                                                { task.status }
+                                                <span className="greyH">
+                                                    { task.percent != '100%' ? ` ${ task.percent }` : '' }
+                                                </span>
+                                            </h3>
                                             <h3 className="blueH">Редактировать</h3>
                                         </div>
                                         <hr/>
